e3f3e22d13ec92724f2f1c8b3750cee58b08bdd0
michelle-marrion <ngamimarrion@gmail.com>
2022-07-23 19:25:27 +0200

    variable en php

---
+++ variable.php
@@ -0,0 +1,31 @@
+<?php
+    $userAge = 17;
+
+    // float
+
+    $price = 15.7;
+
+    // Boolean
+
+    $isAuthor = true;
+    $isAdministrator = false ;
+
+    //null
+
+    $noValue = NULL;
+    $userName = "Ngami";
+    $userEmail = "ngamimarrion@gmail.com";
+
+    $descriptionUser = "Hello, je m'appelle \'$userName et je suis ägé de $userAge";
+
+    //les opérateurs
+    $number = 2 + 4; // $number prend la valeur 6
+    $number = 5 - 1; // $number prend la valeur 4
+    $number = 3 * 5; // $number prend la valeur 15
+    $number = 10 / 2; // $number prend la valeur 5
+
+    // Allez on rajoute un peu de difficulté
+    $number = 3 * 5 + 1; // $number prend la valeur 16
+    $number = (1 + 2) * 2; // $number prend la valeur 6
+
+?>
